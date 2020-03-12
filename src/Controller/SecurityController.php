d31d871154eafeb616c30895e39dd26d8a3d1cb1
moeti <moetichentuf@hotmail.com>
2020-03-12 16:58:52 +0100

    fixed log out page

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Controller;
-
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,13 +10,14 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class SecurityController extends AbstractController
 {
     /**
-     * @Route("/login", name="app_login")
+     * @Route("/", name="app_login")
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
+
+        if ($this->getUser()) {
+             return $this->redirectToRoute('customer');
+         }
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
